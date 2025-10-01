13b9be0614d98c2d779e266a8edade7a4324005e
Pipenko Denys <denzer2281488@gmail.com>
2025-10-01 17:22:42 +0300

    class scanner

--- Scanner.cpp
+++ Scanner.cpp
@@ -1,1 +1,18 @@
 #include "Scanner.h"
+
+Scanner::Scanner()
+{
+}
+
+void Scanner::setText(string text)
+{
+}
+
+string Scanner::getText() const
+{
+    return string();
+}
+
+void Scanner::scanFile(string fileName)
+{
+}
--- Scanner.h
+++ Scanner.h
@@ -1,5 +1,17 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
 class Scanner
 {
+protected:
+	string text;
+public:
+	Scanner();
+	void setText(string text);
+	string getText()const;
+	void scanFile(string fileName);
 };
 
